56bded08f478ae918e0f553e3c264aac150438b5
Nickolas77 <63670595+Nickolas77@users.noreply.github.com>
2020-04-15 23:55:01 +0300

    Update fuck.js
    не понятна 22 и 26 строка

--- fuck.js
+++ fuck.js
@@ -1,3 +1,26 @@
 const fuck = "Fuck this worlk";
 
-s
+var string = "total";
+console.log(string);
+console.log(9/0);
+console.log("toal"*5);
+let comenting;
+console.log(comenting);
+let persone = {
+
+    name: "Nici",
+    age: 43,
+    isMarried: false
+
+}
+
+console.log(persone.isMarried);
+
+ // -to string
+ // 1) способ
+
+ console.log(typeof(string(4)));
+
+ // 2) способ
+
+ console.log(typeof("" + false));
